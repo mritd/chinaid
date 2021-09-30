6cd68390764b007965cbfe386d146095d8d5c802
mritd <mritd@linux.com>
2021-09-30 10:25:15 +0800

    fix(name): fix name gen
    fix name gen
    
    Signed-off-by: mritd <mritd@linux.com>

--- generator_name.go
+++ generator_name.go
@@ -7,5 +7,5 @@ import (
 
 // Name 返回中国姓名，姓名已经尽量返回常用姓氏和名字
 func Name() string {
-	return metadata.LastName[rand.Intn(len(metadata.LastName))] + metadata.FirstName[rand.Intn(len(metadata.LastName))]
+	return metadata.LastName[rand.Intn(len(metadata.LastName))] + metadata.FirstName[rand.Intn(len(metadata.FirstName))]
 }
